3c08dba3e8daf2c9865d50d4b8fe4595c0076bbd
Husin <Muhamad.husin0412@gmail.com>
2019-09-02 20:51:29 +0700

    booking step two

--- app/src/main/java/com/husin/bokingkangbarers/Common/Common.java
+++ app/src/main/java/com/husin/bokingkangbarers/Common/Common.java
@@ -1,8 +1,13 @@
 package com.husin.bokingkangbarers.Common;
 
+import com.husin.bokingkangbarers.Model.Salon;
 import com.husin.bokingkangbarers.Model.User;
 
 public class Common {
+
+    public static final String KEY_ENABLE_BUTTON_NEXT = "ENABLE_BUTTON_NEXT";
+    public static final String KEY_SALON_STORE = "SALON_SAVE";
     public static String IS_LOGIN = "isLogin";
     public static User currentUser;
+    public static Salon currentSalon;
 }
---
+++ app/src/main/java/com/husin/bokingkangbarers/Interface/IRecyclerItemSelectedListener.java
@@ -0,0 +1,9 @@
+package com.husin.bokingkangbarers.Interface;
+
+import android.view.View;
+
+public interface IRecyclerItemSelectedListener {
+    void onItemSelectedListener(View view,int pos);
+
+
+}
